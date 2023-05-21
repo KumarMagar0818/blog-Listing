ada2f6e0e25f4f178cbf3216e8a0bba5fcf2fba4
KumarMagar0818 <112273880+KumarMagar0818@users.noreply.github.com>
2023-05-21 14:19:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import MyDiv from './components/home';
+
+function App() {
+  return (
+    <div className="App">
+      <MyDiv></MyDiv>
+    </div>
+  );
+}
+
+export default App;
